ee196a2021f9d25382b83125bf02eaaa2d117ff5
ljpegal111 <153922429+ljpegal111@users.noreply.github.com>
2023-12-15 06:02:20 -0800

    Add files via upload

---
+++ app.test.js
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-libriary/react';
+import App from './app';
+
+test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+});
